0ec44169ec1a22dbb09dff13aba6c0bc149aa41f
Copter <Pannawich.a@mail.bbvc.ac.th>
2023-06-23 10:58:00 +0700

    movie detail

---
+++ README.md
@@ -0,0 +1,14 @@
+## โปรแกรมแสดงโปสเตอร์หนัง
+ผู้พัฒนา : นาย ธนวัฒน์ พลบุญ โทร0613381033
+
+
+## ขั้นตอนการอัปโหลดไฟล์
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/boatxtream/next13.4-movies.git
+git push -u origin main
+
+## get Api
+https://api.themoviedb.org/3/movie/popular?api_key=d3e9c2f7f664d95671153df6a8374554
---
+++ app/movie/[id]/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image';
+
+type Props = {
+    params: any;
+}
+
+export default async function MovieDetail({params}: Props) {
+  // const key = process.env.NEXT_PUBLIC_API_KEY;
+  const {id} = params;
+  const imagePath = "https://image.tmdb.org/t/p/original"
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=fb8fca6b6e4a57e7034028fd5c7d9179`,{next:{revalidate:10}}
+  );
+  const res = await data.json();
+  return (
+    <div>
+      <div>
+        รายละเอียดของหนัง: 
+        </div>
+      <div>
+      <h2 className=' text-4xl mt-20'>ชื่อหนัง: {res.title}</h2>
+      <h2 className=' text-4xl mt-5'>ความยาว: {res.runtime}</h2>
+      <div className='flex bg-center'>
+      <Image 
+      src={imagePath + res.backdrop_path} 
+      width={700} 
+      height={700}
+      alt='movie'
+      />
+      <p>{res.overview}</p>
+      </div>
+    </div>
+    </div>
+
+  )
+}
